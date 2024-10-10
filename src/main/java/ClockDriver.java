e0addab8b47ebd0ac5be2e26414fc86de5cdae42
paulomiim <paulomimehta0208@yahoo.com>
2024-10-10 12:16:56 -0700

    Update ClockDriver.java
    completed clock driver

--- src/main/java/ClockDriver.java
+++ src/main/java/ClockDriver.java
@@ -1,3 +1,5 @@
+package src.main.java;
+
 
 public class ClockDriver
 {
@@ -14,7 +16,44 @@ public class ClockDriver
         //  * Tick test for 01:59 to 02:00
         //  * Tick test for 09:59 to 10:00
         //  * Tick test for 23:59 to 00:00
+        ClockDisplay test1 = new ClockDisplay();
+        String test1Output = test1.getTime();
+        System.out.println("Empty con. test - " + test1Output);
         
+        ClockDisplay test2 = new ClockDisplay(3,45);
+        String test2Output = test2.getTime();
+        System.out.println("2 Arg. Con. Test - " + test2Output);
+        
+        ClockDisplay tickTest1 = new ClockDisplay(3, 32);
+        System.out.println ("\tTick Test 1 PreTick - " + tickTest1.getTime());
+        tickTest1.timeTick();
+        System.out.println("\t Test Test 1 PostTick" + tickTest1.getTime());
+        
+        tickTest1 = new ClockDisplay(3, 9);
+                System.out.println ("\tTick Test 1 PreTick - " + tickTest1.getTime());
+        tickTest1.timeTick();
+        System.out.println("\t Test Test 1 PostTick" + tickTest1.getTime());
+        
+        tickTest1 = new ClockDisplay(1, 59);
+                System.out.println ("\tTick Test 1 PreTick - " + tickTest1.getTime());
+        tickTest1.timeTick();
+        System.out.println("\t Test Test 1 PostTick" + tickTest1.getTime());
+        
+        tickTest1 = new ClockDisplay(9, 59);
+                System.out.println ("\tTick Test 1 PreTick - " + tickTest1.getTime());
+        tickTest1.timeTick();
+        System.out.println("\t Test Test 1 PostTick" + tickTest1.getTime());
+        
+        tickTest1 = new ClockDisplay(23, 59);
+                System.out.println ("\tTick Test 1 PreTick - " + tickTest1.getTime());
+        tickTest1.timeTick();
+        System.out.println("\t Test Test 1 PostTick" + tickTest1.getTime());
+        
+        
+        
+        
+        
+
         
         //Implement tests for your ClockDisplaySeconds
         //  * Test that you can do the basics
@@ -27,6 +66,26 @@ public class ClockDriver
         //  * Tick test for 01:00:59 to 01:01:00
         //  * Tick test for 01:59:59 to 02:00:00
         //  * Tick test for 23:59:59 to 00:00:00
+        
+        ClockDisplaySeconds tickTest2 = new ClockDisplaySeconds(3, 32, 59);
+        System.out.println ("\tTick Test 1 PreTick - " + tickTest2.getTime());
+        tickTest1.timeTick();
+        System.out.println("\t Test Test 1 PostTick" + tickTest2.getTime());
+        
+        tickTest2 = new ClockDisplaySeconds(1, 0, 59);
+        System.out.println ("\tTick Test 1 PreTick - " + tickTest2.getTime());
+        tickTest2.timeTick();
+        System.out.println("\t Test Test 1 PostTick" + tickTest2.getTime());
+        
+        tickTest2 = new ClockDisplaySeconds(1, 59, 59);
+        System.out.println ("\tTick Test 1 PreTick - " + tickTest2.getTime());
+        tickTest2.timeTick();
+        System.out.println("\t Test Test 1 PostTick" + tickTest2.getTime());
+        
+        tickTest2 = new ClockDisplaySeconds(23, 59, 59);
+        System.out.println ("\tTick Test 1 PreTick - " + tickTest2.getTime());
+        tickTest2.timeTick();
+        System.out.println("\t Test Test 1 PostTick" + tickTest2.getTime());
         
         /*CHALLENGE*/
         //Implement tests for your ClockDisplay12Hour
